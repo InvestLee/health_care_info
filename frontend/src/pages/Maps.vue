7f790815969ac045314455d2ecda86db219134c9
InvestLee <dlwlgud216@nate.com>
2024-10-15 23:33:12 +0900

    프론트엔드 틀 추가

---
+++ frontend/src/pages/Maps.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id = "map">
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    // 네이버 지도 API 로드
+    const script = document.createElement("script");
+    script.src =
+      "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId="+process.env.VUE_APP_API_KEY;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      // 네이버 지도 생성
+      new window.naver.maps.Map("map", {
+        center: new window.naver.maps.LatLng(37.5670135, 126.9783740),
+        zoom: 10
+      });
+    };
+  }
+}
+</script>
+<style>
+#map {
+  min-height: calc(100vh - 123px);
+}
+</style>
